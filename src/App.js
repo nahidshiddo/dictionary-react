5875afaff50b7d59bb35ff2b6133d2eaa06a5ff8
Nahid Shiddo <nahid.shiddo@gmail.com>
2022-07-02 20:01:30 -0400

    Added search engine form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Nahid</small>
+        </footer>
+      </div>
+    </div>
+  );
 }
